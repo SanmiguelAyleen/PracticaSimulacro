a4acc6373ed9db871cfadb037406843f5c8e1d35
SanmiguelAyleen <ayleen.dessiresanmiguel@gmail.com>
2025-06-18 01:32:36 -0500

    Practica Simulacro

---
+++ src/main/java/com/mycompany/practicasimulacro/PracticaSimulacro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.practicasimulacro;
+
+/**
+ *
+ * @author aylee
+ */
+public class PracticaSimulacro {
+
+    public static void main(String[] args) {
+        //ESTO ES UN SIMULACROOOOOOOO
+        System.out.println("Hello World!");
+
+        ClaseUno uno = new ClaseUno();
+        System.out.println("NOMBRE: " + uno.nombre());
+    }
+}
